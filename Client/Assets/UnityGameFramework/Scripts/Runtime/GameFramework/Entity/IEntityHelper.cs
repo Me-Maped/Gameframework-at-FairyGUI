438890adce4f88b7e9e924458e4a13026d9aa41f
maped <13397244375jjx@gmail.com>
2024-12-31 20:41:16 +0800

    修复Entity重复释放资源问题

--- Client/Assets/UnityGameFramework/Scripts/Runtime/GameFramework/Entity/IEntityHelper.cs
+++ Client/Assets/UnityGameFramework/Scripts/Runtime/GameFramework/Entity/IEntityHelper.cs
@@ -26,6 +26,7 @@
         /// </summary>
         /// <param name="entityAsset">要释放的实体资源。</param>
         /// <param name="entityInstance">要释放的实体实例。</param>
-        void ReleaseEntity(object entityAsset, object entityInstance);
+        /// <param name="releaseSource">是否需要释放原资源</param>
+        void ReleaseEntity(object entityAsset, object entityInstance, bool releaseSource);
     }
 }
